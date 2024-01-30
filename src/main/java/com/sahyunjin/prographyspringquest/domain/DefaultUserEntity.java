d1858ddd0f5d45aec4dd5443d91152931b17460f
sahyunjin <tkguswls1106@naver.com>
2024-01-31 01:22:06 +0900

    fix: User의 created_at 포맷 미적용 오류 해결

--- src/main/java/com/sahyunjin/prographyspringquest/domain/DefaultUserEntity.java
+++ src/main/java/com/sahyunjin/prographyspringquest/domain/DefaultUserEntity.java
@@ -27,6 +27,7 @@ public abstract class DefaultUserEntity {
 
     @PrePersist  // 해당 엔티티를 저장하기 이전에 실행된다.
     public void onPrePersist(){
+        this.created_at = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withLocale(Locale.forLanguageTag("ko")));
         this.updated_at = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").withLocale(Locale.forLanguageTag("ko")));
     }
 
