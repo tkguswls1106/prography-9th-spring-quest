aa3424795d54ec86bc2b4cbb087de1ab4199c8a7
sahyunjin <tkguswls1106@naver.com>
2024-01-31 00:39:11 +0900

    feat: faker 데이터로 초기화 기능 작성 완료.

--- src/main/java/com/sahyunjin/prographyspringquest/controller/UserController.java
+++ src/main/java/com/sahyunjin/prographyspringquest/controller/UserController.java
@@ -1,7 +1,13 @@
 package com.sahyunjin.prographyspringquest.controller;
 
+import com.sahyunjin.prographyspringquest.dto.faker.FakerRequestDto;
+import com.sahyunjin.prographyspringquest.response.ApiResponse;
+import com.sahyunjin.prographyspringquest.response.ResponseCode;
 import com.sahyunjin.prographyspringquest.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,5 +17,9 @@ public class UserController {
     private final UserService userService;
 
 
-
+    @GetMapping("/init")
+    public ResponseEntity initUsers(@RequestBody FakerRequestDto fakerRequestDto) {
+        userService.initFakeUsers(fakerRequestDto);
+        return ApiResponse.toResponseEntity(ResponseCode.SUCCESS);
+    }
 }
