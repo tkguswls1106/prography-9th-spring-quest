3e0f6a227e907a5abaa94b015cf075d88b163b97
sahyunjin <tkguswls1106@naver.com>
2024-01-31 15:42:14 +0900

    remove: 미사용 Setter 어노테이션 import 제거

--- src/main/java/com/sahyunjin/prographyspringquest/dto/user/PageResponseDto.java
+++ src/main/java/com/sahyunjin/prographyspringquest/dto/user/PageResponseDto.java
@@ -3,7 +3,6 @@ package com.sahyunjin.prographyspringquest.dto.user;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.List;
 
