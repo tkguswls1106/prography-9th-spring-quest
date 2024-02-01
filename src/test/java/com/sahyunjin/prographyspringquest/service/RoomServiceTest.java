4788f231b2beb98ae7d3586524a5e253020a809d
sahyunjin <tkguswls1106@naver.com>
2024-02-02 02:16:48 +0900

    test: RoomService의 findRooms 메소드 단위 테스트 코드 추가 및 성공

--- src/test/java/com/sahyunjin/prographyspringquest/service/RoomServiceTest.java
+++ src/test/java/com/sahyunjin/prographyspringquest/service/RoomServiceTest.java
@@ -6,6 +6,7 @@ import com.sahyunjin.prographyspringquest.domain.user.User;
 import com.sahyunjin.prographyspringquest.domain.user.UserJpaRepository;
 import com.sahyunjin.prographyspringquest.domain.userroom.UserRoom;
 import com.sahyunjin.prographyspringquest.domain.userroom.UserRoomJpaRepository;
+import com.sahyunjin.prographyspringquest.dto.room.RoomPageResponseDto;
 import com.sahyunjin.prographyspringquest.dto.room.RoomSaveRequestDto;
 import com.sahyunjin.prographyspringquest.service.logic.RoomServiceLogic;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,9 +16,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.*;
@@ -64,5 +69,28 @@ public class RoomServiceTest {
         verify(userRoomJpaRepository, times(1)).existsByUserId(anyInt());
         verify(roomJpaRepository, times(1)).save(any(Room.class));
         verify(userRoomJpaRepository, times(1)).save(any(UserRoom.class));
+    }
+
+    @Test
+    @DisplayName("방 전체 조회_Test")
+    void findRooms_test() {
+
+        // given
+        Integer size = 10;
+        Integer page = 0;
+        Pageable pageable = PageRequest.of(page, size, Sort.by("id").ascending());
+
+        List<Room> rooms = new ArrayList<>();
+        rooms.add(room);
+        Page<Room> roomPage = new PageImpl<>(rooms);
+        when(roomJpaRepository.findAll(pageable)).thenReturn(roomPage);
+
+        // when
+        RoomPageResponseDto roomPageResponseDto = roomServiceLogic.findRooms(size, page);
+
+        // then
+        assertThat(roomPageResponseDto.getTotalElements()).isEqualTo(1);
+        assertThat(roomPageResponseDto.getTotalPages()).isEqualTo(1);
+        assertThat(roomPageResponseDto.getRoomList().get(0).getId()).isEqualTo(1);
     }
 }
