07da8cd72a1be7ec74183eea25dfda655b2f35f1
sahyunjin <tkguswls1106@naver.com>
2024-02-01 20:35:41 +0900

    fix: 문제에 맞추어 http 실제 상태 코드를 200으로 고정

--- src/main/java/com/sahyunjin/prographyspringquest/response/ApiResponse.java
+++ src/main/java/com/sahyunjin/prographyspringquest/response/ApiResponse.java
@@ -1,6 +1,7 @@
 package com.sahyunjin.prographyspringquest.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.sahyunjin.prographyspringquest.response.responseitem.StatusItem;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
@@ -21,7 +22,7 @@ public class ApiResponse<T> {
     // response result(실질적 데이터)가 없을때
     public static ResponseEntity<ApiResponse> toResponseEntity(ResponseCode responseCode) {
         return ResponseEntity
-                .status(responseCode.getHttpStatus())
+                .status(200)  // 원래는 '.status(responseCode.getHttpStatus())'로 작성해야하지만, 문제에 맞추어 모든 http 실제 상태코드는 200으로 반환하도록 하겠다.
                 .body(ApiResponse.builder()
                         .code(responseCode.getHttpStatus())
                         .message(responseCode.getMessage())
@@ -32,7 +33,7 @@ public class ApiResponse<T> {
     // response result(실질적 데이터)가 있을때
     public static <T> ResponseEntity<ApiResponse<T>> toResponseEntity(ResponseCode responseCode, T result) {
         return ResponseEntity
-                .status(responseCode.getHttpStatus())
+                .status(200)  // 원래는 '.status(responseCode.getHttpStatus())'로 작성해야하지만, 문제에 맞추어 모든 http 실제 상태코드는 200으로 반환하도록 하겠다.
                 .body(ApiResponse.<T>builder()
                         .code(responseCode.getHttpStatus())
                         .message(responseCode.getMessage())
@@ -44,7 +45,7 @@ public class ApiResponse<T> {
     // response result(실질적 데이터)와 header가 있을때
     public static <T> ResponseEntity<ApiResponse<T>> toResponseEntity(ResponseCode responseCode, MultiValueMap<String, String> header, T result) {
         return ResponseEntity
-                .status(responseCode.getHttpStatus())
+                .status(200)  // 원래는 '.status(responseCode.getHttpStatus())'로 작성해야하지만, 문제에 맞추어 모든 http 실제 상태코드는 200으로 반환하도록 하겠다.
                 .header(String.valueOf(header))
                 .body(ApiResponse.<T>builder()
                         .code(responseCode.getHttpStatus())
